e80743faf1fdc4b3d2b58b6cd6f47418faed5513
Higor Celante <higor.celante@gmail.com>
2019-08-29 14:30:26 -0300

    lendo arquivo corretamente

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import sys
-import TuringMachine
+#import TuringMachine
 
 def main():
         auxList = []
+        #auxaux =[]
         with open((sys.argv[1]), "r") as f:
+                #for i in f:
+                #        if i == '\n':
+                #                auxList.append(auxaux)
+                #        if i != ' ':
+                #                auxaux.append(i)
                 auxList = [line.strip().split(" ") for line in f]
-        # leu o arquivo e cortou
-        # linha 1 alfabeto de entrada
-        # linha 2 fita
-        # linha 3 simbolo que representa o espaço em branco (padrao: B)
-        # linha 4 estados
-        # linha 5 estado inicial
-        # linha 6 conjunto de estados finais
-        # quantidade de fitas 
-
-        
+        for line in auxList:
+                print(line)
+        #print (auxList)
 if __name__ == "__main__":
   main()
